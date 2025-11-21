3908fee4207b6df872caaec84c242dc5e086b5ea
rohan <rohanverma031@gmail.com>
2025-11-21 20:32:56 +0530

    feat: added http router

---
+++ config/router/router.go
@@ -0,0 +1,32 @@
+package router
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+	"github.com/rs/zerolog/log"
+)
+
+func NewHTTPRouter() *chi.Mux {
+	log.Info().Msg("adding application mux")
+	mux := chi.NewMux()
+
+	mux.Use(middleware.RealIP)
+	mux.Use(middleware.StripSlashes)
+	mux.Use(middleware.Heartbeat("/health"))
+
+	allowedOrigins := []string{
+		"https://accounts.adgytec.in",
+	}
+
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   allowedOrigins,
+		AllowedMethods:   []string{"GET", "POST"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	return mux
+}
