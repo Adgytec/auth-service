d85c17e223871e753963027e25f975d42e28f797
rohan <rohanverma031@gmail.com>
2025-11-22 20:07:42 +0530

    update: added context to storage methods

--- config/storage/storage.go
+++ config/storage/storage.go
@@ -1,24 +1,27 @@
 package storage
 
-import "errors"
+import (
+	"context"
+	"errors"
+)
 
 type Storage interface {
-	Get(sessionID string) (string, error)
-	New(userID string) (string, error)
-	Delete(sessionID string) error
+	Get(ctx context.Context, sessionID string) (string, error)
+	New(ctx context.Context, userID string) (string, error)
+	Delete(ctx context.Context, sessionID string) error
 }
 
 type stubStorage struct{}
 
-func (s *stubStorage) Get(sessionID string) (string, error) {
+func (s *stubStorage) Get(ctx context.Context, sessionID string) (string, error) {
 	return "", errors.New("not implemented")
 }
 
-func (s *stubStorage) New(userID string) (string, error) {
+func (s *stubStorage) New(ctx context.Context, userID string) (string, error) {
 	return "", errors.New("not implemented")
 }
 
-func (s *stubStorage) Delete(sessionID string) error {
+func (s *stubStorage) Delete(ctx context.Context, sessionID string) error {
 	return errors.New("not implemented")
 }
 
