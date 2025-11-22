07bdd0663cf7fd92dd8c83cd43c8624d12251ee1
rohan <rohanverma031@gmail.com>
2025-11-22 20:00:28 +0530

    feat: added stub storage

--- config/storage/storage.go
+++ config/storage/storage.go
@@ -1,7 +1,27 @@
 package storage
 
+import "errors"
+
 type Storage interface {
 	Get(sessionID string) (string, error)
 	New(userID string) (string, error)
 	Delete(sessionID string) error
+}
+
+type stubStorage struct{}
+
+func (s *stubStorage) Get(sessionID string) (string, error) {
+	return "", errors.New("not implemented")
+}
+
+func (s *stubStorage) New(userID string) (string, error) {
+	return "", errors.New("not implemented")
+}
+
+func (s *stubStorage) Delete(sessionID string) error {
+	return errors.New("not implemented")
+}
+
+func New() Storage {
+	return &stubStorage{}
 }
