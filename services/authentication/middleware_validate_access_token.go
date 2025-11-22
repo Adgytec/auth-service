0921093aba6a1088f4ccc3ebf849073591b9d4fb
rohan <rohanverma031@gmail.com>
2025-11-22 21:01:54 +0530

    feat: added method impl for new session

---
+++ services/authentication/middleware_validate_access_token.go
@@ -0,0 +1,85 @@
+package authentication
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+
+	"github.com/Adgytec/auth-service/utils/core"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+type authContextKey string
+
+const (
+	authContextKeyUserID authContextKey = "userID"
+)
+
+// validateAccessToken() parses and validate jwt token and return user id
+func (m *authServiceMux) validateAccessToken(accessToken string) (uuid.UUID, error) {
+	jwtToken, jwtParseErr := jwt.Parse(accessToken, m.jwtKeyfunc.Keyfunc)
+	if jwtParseErr != nil {
+		if errors.Is(jwtParseErr, jwt.ErrTokenMalformed) ||
+			errors.Is(jwtParseErr, jwt.ErrTokenSignatureInvalid) ||
+			errors.Is(jwtParseErr, jwt.ErrTokenExpired) ||
+			errors.Is(jwtParseErr, jwt.ErrTokenNotValidYet) {
+			return uuid.Nil, errors.New("invalid access token")
+		}
+
+		return uuid.Nil, jwtParseErr
+	}
+
+	invalidTokenErr := errors.New("invalid token")
+
+	if !jwtToken.Valid {
+		return uuid.Nil, invalidTokenErr
+	}
+
+	// get username from claims
+	claims, claimsOK := jwtToken.Claims.(jwt.MapClaims)
+	if !claimsOK {
+		return uuid.Nil, invalidTokenErr
+	}
+
+	// cognito access token contains claim 'username' for user's username field
+	username, usernameOK := claims["username"].(string)
+	if !usernameOK {
+		return uuid.Nil, invalidTokenErr
+	}
+
+	userID := core.GetUserIDFromUsername(username)
+	return userID, nil
+}
+
+// return scheme, value, error
+func parseAuthHeader(authHeader string) (string, string, error) {
+	authHeaderSlice := strings.Fields(authHeader)
+	if len(authHeaderSlice) != 2 {
+		return "", "", errors.New("invalid auth header")
+	}
+
+	scheme := strings.ToLower(authHeaderSlice[0])
+	value := authHeaderSlice[1]
+	return scheme, value, nil
+}
+
+func (m *authServiceMux) validateAndGetUserIDCtx(r *http.Request) (string, error) {
+	authHeader := r.Header.Get(authorizationHeaderKey)
+	scheme, value, authErr := parseAuthHeader(authHeader)
+	if authErr != nil {
+		return "", authErr
+	}
+
+	switch scheme {
+	case authorizationSchemeBearer:
+		userID, validateErr := m.validateAccessToken(value)
+		if validateErr != nil {
+			return "", validateErr
+		}
+		return userID.String(), nil
+
+	default:
+		return "", errors.New("unsupported auth scheme")
+	}
+}
--- services/authentication/new_session.go
+++ services/authentication/new_session.go
@@ -1,5 +1,46 @@
 package authentication
 
-import "net/http"
+import (
+	"context"
+	"net/http"
+	"time"
+)
 
-func (m *authServiceMux) newSession(w http.ResponseWriter, r *http.Request) {}
+const (
+	authorizationHeaderKey    = "Authorization"
+	authorizationSchemeBearer = "bearer"
+)
+
+var sessionCookieDuration = 24 * 15 * time.Hour
+
+func (s *authService) newSession(ctx context.Context, userID string) (string, error) {
+	return s.s.New(ctx, userID)
+}
+
+func (m *authServiceMux) newSession(w http.ResponseWriter, r *http.Request) {
+	userID, err := m.validateAndGetUserIDCtx(r)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	sessionID, err := m.service.newSession(r.Context(), userID)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	sessionCookie := http.Cookie{
+		Name:     cookieKeySessionID,
+		Value:    sessionID,
+		Path:     "/",
+		Domain:   ".adgytec.in",
+		Expires:  time.Now().Add(sessionCookieDuration),
+		HttpOnly: true,
+		Secure:   true,
+		SameSite: http.SameSiteNoneMode,
+	}
+
+	http.SetCookie(w, &sessionCookie)
+	w.WriteHeader(http.StatusCreated)
+}
