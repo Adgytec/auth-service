c7756d0daa28a31c5d3462de9c2a25de7fd5f79b
rohan <rohanverma031@gmail.com>
2025-11-22 20:10:15 +0530

    feat: added get session info method impl

--- services/authentication/get_session_info.go
+++ services/authentication/get_session_info.go
@@ -2,10 +2,28 @@ package authentication
 
 import (
 	"context"
+	"errors"
 
 	"github.com/Adgytec/service-protos/auth/v1"
 )
 
+func (s *authService) getSessionInfo(ctx context.Context, sessionID string) (string, error) {
+	return s.s.Get(ctx, sessionID)
+}
+
 func (a *authServicePC) GetSessionInfo(ctx context.Context, req *auth.GetSessionInfoRequest) (*auth.GetSessionInfoResponse, error) {
-	return nil, nil
+	if req == nil {
+		return nil, errors.New("session id not present")
+	}
+
+	userID, sessionErr := a.service.getSessionInfo(ctx, req.SessionId)
+	if sessionErr != nil {
+		return nil, sessionErr
+	}
+
+	sessionValid := userID != ""
+	return &auth.GetSessionInfoResponse{
+		Valid:  sessionValid,
+		UserId: userID,
+	}, nil
 }
