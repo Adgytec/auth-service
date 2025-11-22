f5d18688662ae6a72b9a0d12756c612af7076d0c
rohan <rohanverma031@gmail.com>
2025-11-22 21:20:33 +0530

    update: removed unnecessary call to load env in core/id.go

--- utils/core/id.go
+++ utils/core/id.go
@@ -6,7 +6,6 @@ import (
 	"sync"
 
 	"github.com/google/uuid"
-	"github.com/joho/godotenv"
 	"github.com/rs/zerolog/log"
 )
 
@@ -18,13 +17,6 @@ var (
 // this method panics if id namespace is not found
 func getIDNamespace() uuid.UUID {
 	idOnce.Do(func() {
-		err := godotenv.Load()
-		if err != nil {
-			log.Warn().
-				Err(err).
-				Msg("failed to load .env")
-		}
-
 		namespaceString := os.Getenv("ID_NAMESPACE")
 		namespaceVal, namespaceErr := uuid.Parse(namespaceString)
 		if namespaceErr != nil {
